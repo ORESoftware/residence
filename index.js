d2fdd82fffadde620f2061d22374169437545444
Olegzandr <alex@oresoftware.com>
2017-04-13 10:45:57 -0700

    dir

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-/**
- * Created by amills001c on 3/1/16.
- */
-
-
 
 const path = require('path');
 const fs = require('fs');
 
 module.exports = {
 
-
     findProjectRoot: function findRoot(pth) {
 
         var possiblePkgDotJsonPath = path.resolve(String(pth) + '/package.json');
 
         try {
-            fs.statSync(possiblePkgDotJsonPath).isFile();
-            return pth;
+            if(fs.statSync(possiblePkgDotJsonPath).isFile()){
+              return pth;
+            }
+           else{
+                throw ' => a "package.json" directory? alrighty then...'
+            }
         }
         catch (err) {
             var subPath = path.resolve(String(pth) + '/../');
