ef793f477cc66398f3dbba9d55c262872805ffde
Olegzandr <alex@oresoftware.com>
2017-06-16 20:07:41 -0700

    minor refactor

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-
 const path = require('path');
 const fs = require('fs');
 
-module.exports = {
+exports.findProjectRoot = function findRoot(pth) {
 
-    findProjectRoot: function findRoot(pth) {
+  var possiblePkgDotJsonPath = path.resolve(String(pth) + '/package.json');
 
-        var possiblePkgDotJsonPath = path.resolve(String(pth) + '/package.json');
-
-        try {
-            if(fs.statSync(possiblePkgDotJsonPath).isFile()){
-              return pth;
-            }
-           else{
-                throw ' => a "package.json" directory? alrighty then...'
-            }
-        }
-        catch (err) {
-            var subPath = path.resolve(String(pth) + '/../');
-            if (subPath === pth) {
-                return null;
-            }
-            else {
-                return findRoot(subPath);
-            }
-        }
-
+  try {
+    if (fs.statSync(possiblePkgDotJsonPath).isFile()) {
+      return pth;
     }
+    else {
+      throw ' => a "package.json" <i>directory</i>? alrighty then...'
+    }
+  }
+  catch (err) {
+    var subPath = path.resolve(String(pth) + '/../');
+    if (subPath === pth) {
+      return null;
+    }
+    else {
+      return findRoot(subPath);
+    }
+  }
 
 };
