2022201abb0c988a56025d920848ae5e397e359c
Olegzandr <alex@oresoftware.com>
2017-06-20 11:26:58 -0700

    pdev:set

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const path = require('path');
-const fs = require('fs');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var path = require('path');
+var fs = require('fs');
 exports.findProjectRoot = function findRoot(pth) {
-
-  var possiblePkgDotJsonPath = path.resolve(String(pth) + '/package.json');
-
-  try {
-    if (fs.statSync(possiblePkgDotJsonPath).isFile()) {
-      return pth;
+    var possiblePkgDotJsonPath = path.resolve(String(pth) + '/package.json');
+    try {
+        if (fs.statSync(possiblePkgDotJsonPath).isFile()) {
+            return pth;
+        }
+        else {
+            throw ' => a "package.json" <i>directory</i>? alrighty then...';
+        }
     }
-    else {
-      throw ' => a "package.json" <i>directory</i>? alrighty then...'
+    catch (err) {
+        var subPath = path.resolve(String(pth) + '/../');
+        if (subPath === pth) {
+            return null;
+        }
+        else {
+            return findRoot(subPath);
+        }
     }
-  }
-  catch (err) {
-    var subPath = path.resolve(String(pth) + '/../');
-    if (subPath === pth) {
-      return null;
-    }
-    else {
-      return findRoot(subPath);
-    }
-  }
-
 };
